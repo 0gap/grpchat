07b66c95c36d00127dd716429065451d943fb15c
Panagiotis Gourgaris <pgourgaris@gmail.com>
2021-12-04 09:45:01 +0200

    Adding a test

--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/go-cmp/cmp"
+	"learningGo/morestrings"
 	"os"
 )
 
 func main() {
-	fmt.Println("Hello from " + os.Getenv("USER"))
+	fmt.Println(morestrings.ReverseRunes("Hello from " + os.Getenv("USER")))
+	fmt.Println(cmp.Diff("Hello world", "Hello Go"))
 }
