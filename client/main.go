1fdef9698bca871c4d78ba2ed73cf49b2e856062
Panagiotis Gourgaris <pgourgaris@gmail.com>
2021-12-04 14:17:43 +0200

    Simple one channel chat test

---
+++ client/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	lpb "learningGo/proto"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	client lpb.BroadcastClient
+	wait   *sync.WaitGroup
+)
+
+func init() {
+	wait = &sync.WaitGroup{}
+}
+
+func connect(user *lpb.User) error {
+	var streamerror error
+	active := true
+	stream, err := client.CreateStream(context.Background(), &lpb.Connect{
+		User: user, Active: &active,
+	})
+	if err != nil {
+		return fmt.Errorf("Connaction failed: %v", err)
+	}
+
+	wait.Add(1)
+
+	go func(str lpb.Broadcast_CreateStreamClient) {
+		defer wait.Done()
+
+		for {
+			msg, err := str.Recv()
+			if err != nil {
+				streamerror = fmt.Errorf("error receiving msg: %v", err)
+				break
+			}
+			fmt.Printf("Received %v from user %d\n", *msg.Content, *msg.Id)
+		}
+	}(stream)
+	return streamerror
+}
+
+func main() {
+	timestamp := time.Now()
+	done_chan := make(chan int)
+
+	name := flag.String("n", "panos", "The name of the user sending")
+	flag.Parse()
+
+	id := sha256.Sum256([]byte(timestamp.String() + *name))
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Couldn't connect to the service: %v", err)
+	}
+
+	client = lpb.NewBroadcastClient(conn)
+	hxid := hex.EncodeToString(id[:])
+	user := &lpb.User{
+		Id:   &hxid,
+		Name: name,
+	}
+	connect(user)
+
+	wait.Add(1)
+
+	go func() {
+		defer wait.Done()
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			content := scanner.Text()
+			t := timestamp.String()
+			msg := &lpb.
+				Message{Id: user.Id, Content: &content, Timestamp: &t}
+			_, err := client.BroadcastMessage(context.Background(), msg)
+			if err != nil {
+				fmt.Printf("Error Sending message: %v", err)
+				break
+			}
+		}
+	}()
+
+	go func() {
+		wait.Wait()
+		close(done_chan)
+	}()
+
+	<-done_chan
+}
--- hello.go
+++ hello.go
@@ -1,13 +1,89 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
-	"github.com/google/go-cmp/cmp"
-	"learningGo/morestrings"
+	"google.golang.org/grpc"
+	glog "google.golang.org/grpc/grpclog"
+	lpb "learningGo/proto"
+	"log"
+	"net"
 	"os"
+	"sync"
 )
 
+var (
+	grpcLog     glog.LoggerV2
+	server_port = flag.Int("port", 50051, "The server port")
+)
+
+func init() {
+	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
+}
+
+type Connection struct {
+	stream lpb.Broadcast_CreateStreamServer
+	id     string
+	active bool
+	error  chan error
+}
+
+type Server struct {
+	lpb.UnimplementedBroadcastServer
+	Connection []*Connection
+}
+
+func (s *Server) CreateStream(pconn *lpb.Connect, stream lpb.Broadcast_CreateStreamServer) error {
+	conn := &Connection{
+		stream, *pconn.User.Id, true, make(chan error),
+	}
+	s.Connection = append(s.Connection, conn)
+	return <-conn.error
+}
+
+func (s *Server) BroadcastMessage(ctx context.Context, msg *lpb.Message) (*lpb.Close, error) {
+	wait := sync.WaitGroup{}
+	done_chan := make(chan int)
+
+	for _, conn := range s.Connection {
+		wait.Add(1)
+
+		go func(msg *lpb.Message, conn *Connection) {
+			defer wait.Done()
+			if conn.active {
+				err := conn.stream.Send(msg)
+				grpcLog.Info("Sending msg with user id ", *msg.Id, " to: ", conn.stream)
+				if err != nil {
+					grpcLog.Error("Error with Stream: %s - Error: %v", conn.stream, err)
+					conn.active = false
+					conn.error <- err
+				}
+			}
+		}(msg, conn)
+	}
+	go func() {
+		wait.Wait()
+		close(done_chan)
+	}()
+	<-done_chan
+	return &lpb.Close{}, nil
+}
+
 func main() {
-	fmt.Println(morestrings.ReverseRunes("Hello from " + os.Getenv("USER")))
-	fmt.Println(cmp.Diff("Hello world", "Hello Go"))
+	var connections []*Connection
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *server_port))
+	if err != nil {
+		log.Fatalf("error creating the server %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	server := &Server{}
+	server.Connection = connections
+
+	lpb.RegisterBroadcastServer(grpcServer, server)
+	grpcLog.Info("Starting server at :", *server_port)
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
